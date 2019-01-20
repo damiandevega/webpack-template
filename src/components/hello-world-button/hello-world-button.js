e8cb8b4344cae7c8393001726d23c80317e9a27c
Damian de Vega <damiandevega@gmail.com>
2019-01-20 12:14:51 -0500

    added sass loader to config

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -1,8 +1,17 @@
+import './hello-world-button.scss';
+
 class HelloWorldButton {
     render() {
         const button = document.createElement('button');
         button.innerHTML = 'Hello World';
+        button.classList.add('hello-world-button');
         const body = document.querySelector('body');
+        button.onclick = function() {
+            const p = document.createElement('p');
+            p.innerHTML = 'Hello world';
+            p.classList.add('hello-world-text');
+            body.appendChild(p);
+        }
         body.appendChild(button);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,18 @@ module.exports = {
                 use: [
                     "file-loader"
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader", "css-loader"
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", "css-loader", "sass-loader"
+                ]
             }
         ]
     }
