0768c8133bf7d53aa3c3a8a891ee0481e96b9ccb
Damian de Vega <damiandevega@gmail.com>
2019-01-23 11:36:17 -0500

    added webpack dev server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Heading from './components/heading/heading';
 
 const heading = new Heading();
 heading.render();
+
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
