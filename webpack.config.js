335af14925ca1e89072e1c018eee8fa264362c28
Damian de Vega <damiandevega@gmail.com>
2019-01-22 23:52:59 -0500

    added dist folder cleanup on new builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -46,7 +47,8 @@ module.exports = {
     plugins: [
         new UglifyJsPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'styles.[contenthash].css'
-        })
+            filename: "styles.[contenthash].css"
+        }),
+        new CleanWebpackPlugin("dist")
     ]
 }
