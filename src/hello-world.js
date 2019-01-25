6b9e197389547acde5d9fab703406fc1e3f751e6
Damian de Vega <damiandevega@gmail.com>
2019-01-25 10:35:26 -0500

    added multi page configuration to development config

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,6 +1,6 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
 import Heading from './components/heading/heading';
-import React from 'react;'
+import React from 'react';
 
 const heading = new Heading();
 heading.render('hello world');
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -3,9 +3,12 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        "hello-world": "./src/hello-world.js",
+        "kauai": "./src/kauai.js"
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
         publicPath: ""
     },
@@ -57,9 +60,18 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin("dist"),
         new HtmlWebpackPlugin({
+            filename: "hello-world.html",
+            chunks: ["hello-world"],
             title: "Webpack Template",
-            template: "src/index.hbs",
-            description: "webpack starting template code"
+            description: "webpack starting template code",
+            template: "src/page-template.hbs"
+        }),
+        new HtmlWebpackPlugin({
+            filename: "kauai.html",
+            chunks: ["kauai"],
+            title: "Kauai",
+            description: "Kauai page",
+            template: "src/page-template.hbs"
         })
     ]
 }
